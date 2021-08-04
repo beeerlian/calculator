55adbd6c623aef693777e0aa8415594c67e641aa
beeerlian <naufal.berlian.99@gmail.com>
2021-08-04 07:47:11 +0700

    second commit

--- lib/myhomepage.dart
+++ lib/myhomepage.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_calculator_app/model/tombol.dart';
+import 'package:flutter_calculator_app/widgets/tombol.dart';
 import 'package:math_expressions/math_expressions.dart';
 
 class MainBody extends StatefulWidget {
